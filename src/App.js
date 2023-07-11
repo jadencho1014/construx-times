ce6cec0e7a8a90e2bf6d2ed90bd0fa2f0a2e4015
Wonjae Cho <51027177+jadencho1014@users.noreply.github.com>
2023-07-11 02:38:11 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+const URL = "https://hacker-news.firebaseio.com/v0/topstories.json";
 
 function App() {
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => response.data.slice(0, 100))
+      .then((storyIds) =>
+        Promise.all(
+          storyIds.map((storyId) =>
+            axios.get(
+              `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
+            )
+          )
+        )
+      )
+      .then((responses) => responses.map((response) => response.data))
+      .then((stories) => setStories(stories));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Construx Times</h1>
+      <h2>View the top 100 articles from Hacker News!</h2>
+      <ol>
+        {stories.map((story) => (
+          <li key={story.id}>
+            <a href={story.url} target="_blank" rel="noreferrer">
+              {story.title}
+            </a>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
